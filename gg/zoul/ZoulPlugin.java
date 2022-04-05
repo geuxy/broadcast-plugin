c08f9a1be52d098a22b1ca4a30ecc85bbe5b49fa
Geuxy <88702612+Geuxy@users.noreply.github.com>
2022-04-05 20:06:09 +0800

    Update ZoulPlugin.java

--- gg/zoul/ZoulPlugin.java
+++ gg/zoul/ZoulPlugin.java
@@ -9,7 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ZoulPlugin extends JavaPlugin implements CommandExecutor  {
+public class ZoulPlugin extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
